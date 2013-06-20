e01a8fce4dc59136fa9d61b6911a7b49c5532e1c
ratnakarrao-nyros <ratnakarrao_nyros@yahoo.com>
2013-06-20 09:32:17 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anet::Application.config.secret_token = '07cc182f2ccfbd545f1061e82cd5ee7c0e2c194e9e3c2fd435eddb6128f4caa5ec01cc7363395da7e8b5941664fe353cf0da5257e0b4f6a34e0b013b11664fc1'
